2e9d00b09006b13fb4328860648168117af406a8
tanu <dastanushri402@gmail.com>
2024-04-01 01:05:57 +0600

    Enhanced UI interactivity and responsiveness

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Api_key = "283f90719ecc2d0dd3cb316eb553a072";
 
@@ -203,28 +204,26 @@ const Home = () => {
   }, [apiData, forecastData, showWeather]);
 
   return (
-    <div className="h-screen grid place-items-center">
-      <div className="bg-white w-96 p-4 rounded-md">
+    <div className="h-full grid place-items-center bg-[#3944bc] w-full sm:w-11/12 2xl:w-2/4 mx-auto">
+      <div className="w-10/12 py-12 rounded-md">
         <div className="flex items-center justify-between">
           <input
             type="text"
             ref={inputRef}
             placeholder="Enter Your Location"
-            className="text-xl border-b p-1 border-gray-200 font-semibold uppercase flex-1"
+            className="text-[16px] border-b py-3 ps-4 normal-case border-gray-200 font-semibold flex-1 rounded-full"
           />
-          <button onClick={fetchWeather}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/758/758651.png"
-              alt="..."
-              className="w-8"
-            />
+          <button
+            className="ml-2 focus:outline-none bg-white w-12 h-12 flex justify-center items-center rounded-full"
+            onClick={() => {
+              fetchWeather();
+              inputRef.current.value = "";
+            }}
+          >
+            <FaSearch className="text-[16px]" />
           </button>
         </div>
-        <div
-          className={`duration-300 delay-75  overflow-hidden ${
-            showWeather ? "h-[30rem]" : "h-0"
-          }`}
-        >
+        <div className="duration-300 delay-75 overflow-hidden">
           {loading ? (
             <div className="grid place-items-center h-full">
               <img
@@ -235,37 +234,35 @@ const Home = () => {
             </div>
           ) : (
             showWeather && (
-              <div className="text-center flex flex-col gap-6 mt-10">
+              <div className="text-center flex flex-col gap-6 mt-8">
                 {apiData && (
-                  <p className="text-xl font-semibold">
-                    {apiData?.name + "," + apiData?.sys?.country}
-                  </p>
+                  <h3 className="text-3xl text-white font-extrabold">
+                    {apiData?.name + " , " + apiData?.sys?.country}
+                  </h3>
                 )}
                 <img
                   src={showWeather[0]?.img}
                   alt="..."
-                  className="w-52 mx-auto"
+                  className="w-20 mx-auto -mt-2 -mb-4"
                 />
-                <h3 className="text-2xl font-bold text-zinc-800">
+                <h3 className="text-2xl font-bold text-white">
                   {showWeather[0]?.type}
                 </h3>
 
                 {apiData && (
                   <div className="flex flex-col items-center">
-                    <div className="flex justify-center items-center">
-                      <h2 className="text-4xl font-extrabold">
-                        {unit === "metric"
-                          ? apiData?.main?.temp.toFixed(2) + "°C"
-                          : apiData?.main?.temp.toFixed(2) + "°F"}
-                      </h2>
-                      <button
-                        onClick={toggleUnit}
-                        className="text-blue-500 font-semibold ml-4"
-                      >
-                        Toggle Unit
-                      </button>
-                    </div>
-                    <h3 className="text-xl font-bold text-zinc-800 mt-4">
+                    <h2 className="text-2xl font-extrabold text-white mb-4">
+                      {unit === "metric"
+                        ? apiData?.main?.temp.toFixed(2) + "°C"
+                        : apiData?.main?.temp.toFixed(2) + "°F"}
+                    </h2>
+                    <button
+                      onClick={toggleUnit}
+                      className="text-white text-lg border-2 border-white px-6 py-3 rounded-full font-semibold "
+                    >
+                      Toggle Unit
+                    </button>
+                    <h3 className="text-xl font-bold text-white mt-4">
                       Humidity: {apiData?.main?.humidity}%
                     </h3>
                   </div>
@@ -275,29 +272,41 @@ const Home = () => {
           )}
         </div>
         <div className="mt-8">
-          <h2 className="text-lg font-semibold mb-2">5-Day Forecast</h2>
-          <div className="grid grid-cols-1 gap-4">
-            {forecastData.map((item, index) => (
-              <div key={index} className="text-center">
-                <p>{new Date(item.dt * 1000).toLocaleDateString()}</p>
-                <img
-                  src={
-                    WeatherTypes.find(
-                      (weather) => weather.type === item.weather[0].main
-                    )?.img
-                  }
-                  alt="..."
-                  className="w-12 mx-auto"
-                />
-                <p>
-                  Temperature: {item.main.temp.toFixed(2)}&deg;
-                  {unit === "metric" ? "C" : "F"}
-                </p>
-                <p>Humidity: {item.main.humidity}%</p>
-                <p>Weather Type: {item.weather[0].main}</p>
+          {!loading && forecastData.length > 0 && (
+            <>
+              <h2 className="text-3xl font-bold text-center text-white">
+                5-Day Forecast
+              </h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+                {forecastData.map((item, index) => (
+                  <div key={index} className="text-center">
+                    <h3 className="text-xl font-bold text-white">
+                      {new Date(item.dt * 1000).toLocaleDateString()}
+                    </h3>
+                    <img
+                      src={
+                        WeatherTypes.find(
+                          (weather) => weather.type === item.weather[0].main
+                        )?.img
+                      }
+                      alt="..."
+                      className="w-14 mx-auto my-2"
+                    />
+                    <p className="text-[16px] font-semibold text-white">
+                      Temperature : {item.main.temp.toFixed(2)}&deg;
+                      {unit === "metric" ? "C" : "F"}
+                    </p>
+                    <p className="text-[16px] font-semibold text-white mb-1">
+                      Humidity : {item.main.humidity}%
+                    </p>
+                    <p className="text-[16px] font-semibold text-white mb-1">
+                      Weather Type : {item.weather[0].main}
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
+            </>
+          )}
         </div>
       </div>
     </div>
