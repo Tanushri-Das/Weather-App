48e4ba61dd72deb4e43841a1143314d994457a20
tanu <dastanushri402@gmail.com>
2024-04-01 11:59:54 +0600

    Implemented animated transitions to enhance user experience

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useTransition, animated } from "react-spring";
 
 const Api_key = "283f90719ecc2d0dd3cb316eb553a072";
 
@@ -45,6 +46,22 @@ const Home = () => {
     },
   ];
 
+  // Animation transitions for weather data
+  const weatherTransition = useTransition(showWeather, {
+    from: { opacity: 0, transform: "translateY(-20px)" },
+    enter: { opacity: 1, transform: "translateY(0px)" },
+    leave: { opacity: 0, transform: "translateY(-20px)" },
+    config: { duration: 500 },
+  });
+
+  // Animation transitions for forecast data
+  const forecastTransitions = useTransition(forecastData, {
+    from: { opacity: 0, transform: "translateY(-20px)" },
+    enter: { opacity: 1, transform: "translateY(0px)" },
+    leave: { opacity: 0, transform: "translateY(-20px)" },
+    config: { duration: 500 },
+  });
+
   useEffect(() => {
     const cachedData = localStorage.getItem("weatherData");
     if (cachedData) {
@@ -80,6 +97,8 @@ const Home = () => {
               },
             ]);
             setApiData(null);
+            // Clear forecast data
+            setForecastData([]);
           } else {
             setShowWeather(
               WeatherTypes.filter(
@@ -182,7 +201,9 @@ const Home = () => {
       main: {
         ...item.main,
         temp:
-          newUnit === "metric" ? item.main.temp : (item.main.temp * 9) / 5 + 32,
+          newUnit === "metric"
+            ? ((item.main.temp - 32) * 5) / 9
+            : item.main.temp * 1.8 + 32,
       },
     }));
     setForecastData(convertedForecastData);
@@ -204,8 +225,8 @@ const Home = () => {
   }, [apiData, forecastData, showWeather]);
 
   return (
-    <div className="h-full grid place-items-center bg-[#3944bc] w-full sm:w-11/12 2xl:w-2/4 mx-auto">
-      <div className="w-10/12 py-12 rounded-md">
+    <div className="h-full grid place-items-center bg-[#3944bc] w-full sm:w-11/12 2xl:w-2/4 mx-auto md:my-8">
+      <div className="w-10/12 py-10 rounded-md">
         <div className="flex items-center justify-between">
           <input
             type="text"
@@ -225,7 +246,7 @@ const Home = () => {
         </div>
         <div className="duration-300 delay-75 overflow-hidden">
           {loading ? (
-            <div className="grid place-items-center h-full">
+            <div className="grid place-items-center h-full mt-6">
               <img
                 src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png"
                 alt="..."
@@ -233,41 +254,47 @@ const Home = () => {
               />
             </div>
           ) : (
-            showWeather && (
-              <div className="text-center flex flex-col gap-6 mt-8">
-                {apiData && (
-                  <h3 className="text-3xl text-white font-extrabold">
-                    {apiData?.name + " , " + apiData?.sys?.country}
-                  </h3>
-                )}
-                <img
-                  src={showWeather[0]?.img}
-                  alt="..."
-                  className="w-20 mx-auto -mt-2 -mb-4"
-                />
-                <h3 className="text-2xl font-bold text-white">
-                  {showWeather[0]?.type}
-                </h3>
-
-                {apiData && (
-                  <div className="flex flex-col items-center">
-                    <h2 className="text-2xl font-extrabold text-white mb-4">
-                      {unit === "metric"
-                        ? apiData?.main?.temp.toFixed(2) + "°C"
-                        : apiData?.main?.temp.toFixed(2) + "°F"}
-                    </h2>
-                    <button
-                      onClick={toggleUnit}
-                      className="text-white text-lg border-2 border-white px-6 py-3 rounded-full font-semibold "
-                    >
-                      Toggle Unit
-                    </button>
-                    <h3 className="text-xl font-bold text-white mt-4">
-                      Humidity: {apiData?.main?.humidity}%
+            weatherTransition(
+              (style, item) =>
+                item && (
+                  <animated.div
+                    style={style}
+                    className="text-center flex flex-col gap-6 mt-8"
+                  >
+                    {apiData && (
+                      <h3 className="text-3xl text-white font-extrabold">
+                        {apiData?.name + " , " + apiData?.sys?.country}
+                      </h3>
+                    )}
+                    <img
+                      src={showWeather[0]?.img}
+                      alt="..."
+                      className="w-20 mx-auto -mt-2 -mb-4"
+                    />
+                    <h3 className="text-2xl font-bold text-white">
+                      {showWeather[0]?.type}
                     </h3>
-                  </div>
-                )}
-              </div>
+
+                    {apiData && (
+                      <div className="flex flex-col items-center">
+                        <h2 className="text-2xl font-extrabold text-white mb-4">
+                          {unit === "metric"
+                            ? apiData?.main?.temp.toFixed(2) + "°C"
+                            : apiData?.main?.temp.toFixed(2) + "°F"}
+                        </h2>
+                        <button
+                          onClick={toggleUnit}
+                          className="text-white text-lg border-2 border-white px-6 py-3 rounded-full font-semibold "
+                        >
+                          Toggle Unit
+                        </button>
+                        <h3 className="text-xl font-bold text-white mt-4">
+                          Humidity: {apiData?.main?.humidity}%
+                        </h3>
+                      </div>
+                    )}
+                  </animated.div>
+                )
             )
           )}
         </div>
@@ -278,32 +305,39 @@ const Home = () => {
                 5-Day Forecast
               </h2>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
-                {forecastData.map((item, index) => (
-                  <div key={index} className="text-center">
-                    <h3 className="text-xl font-bold text-white">
-                      {new Date(item.dt * 1000).toLocaleDateString()}
-                    </h3>
-                    <img
-                      src={
-                        WeatherTypes.find(
-                          (weather) => weather.type === item.weather[0].main
-                        )?.img
-                      }
-                      alt="..."
-                      className="w-14 mx-auto my-2"
-                    />
-                    <p className="text-[16px] font-semibold text-white">
-                      Temperature : {item.main.temp.toFixed(2)}&deg;
-                      {unit === "metric" ? "C" : "F"}
-                    </p>
-                    <p className="text-[16px] font-semibold text-white mb-1">
-                      Humidity : {item.main.humidity}%
-                    </p>
-                    <p className="text-[16px] font-semibold text-white mb-1">
-                      Weather Type : {item.weather[0].main}
-                    </p>
-                  </div>
-                ))}
+                {forecastTransitions(
+                  (styles, item) =>
+                    item && (
+                      <animated.div
+                        style={styles}
+                        key={item.dt}
+                        className="text-center"
+                      >
+                        <h3 className="text-xl font-bold text-white">
+                          {new Date(item.dt * 1000).toLocaleDateString()}
+                        </h3>
+                        <img
+                          src={
+                            WeatherTypes.find(
+                              (weather) => weather.type === item.weather[0].main
+                            )?.img
+                          }
+                          alt="..."
+                          className="w-14 mx-auto my-2"
+                        />
+                        <p className="text-[16px] font-semibold text-white">
+                          Temperature : {item.main.temp.toFixed(2)}&deg;
+                          {unit === "metric" ? "C" : "F"}
+                        </p>
+                        <p className="text-[16px] font-semibold text-white mb-1">
+                          Humidity : {item.main.humidity}%
+                        </p>
+                        <p className="text-[16px] font-semibold text-white mb-1">
+                          Weather Type : {item.weather[0].main}
+                        </p>
+                      </animated.div>
+                    )
+                )}
               </div>
             </>
           )}
